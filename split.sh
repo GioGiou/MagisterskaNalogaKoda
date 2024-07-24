622d4336ddd7a6f42bd5c08047fb306e8f085649
Jani Suban <jani.suban@gmail.com>
2024-07-24 13:22:54 +0200

    Vizualizacija prvih 80 znakov dna.50MB

--- split.sh
+++ split.sh
@@ -9,8 +9,8 @@ if [ $# -ge 2 ]; then
   echo "Scripta prejem en argument <datoteka>\n"
   exit 0
 fi
-
+cat "../test/dna.50MB" | head "-c 80" >> "../test/dna.50MB.80.txt"
 for ((i=800; i<=35000000;i=i*2)) do
 
-	cat "../test/dna.50MB" | head "-c $i" >> "../test/dna.50MB.$i.txt"
+	cat "$1" | head "-c $i" >> "$1.$i.txt"
 done
