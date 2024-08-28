0f694500daaaeb1bc5f8e743fb7eee119fcfb47b
Jani Suban <giogiou@github.com>
2024-08-28 13:24:42 +0200

    New run

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
 	ST: 2048000 je prekine testiranje, I/O sleep
 */
   for (j = 500; j <= 2000000; j = j*2) {
-    //sleep(10);
+    sleep(10);
     int i;
     double totalTime = 0;
     int totalSize = 0;
@@ -174,7 +174,7 @@ int main(int argc, char **argv) {
     cout<<"\tSize in B:"<<1.0*totalSize/n<<endl;
     cout<<"\tTime in ms:"<<1.0*totalTime/n<<endl;
 
-    sleep(1);
+    sleep(10);
     for (i = 0; i < n; i++) {
 	  cout << i;
 	  cout.flush();
